558048b04032b4b7c70e6dfc2bf61b694c05efc8
TheRusBus <rusty.gates@utexas.edu>
2016-09-14 21:02:59 -0500

    Add files via upload

---
+++ TitanicPlot2.R
@@ -0,0 +1,21 @@
+require("jsonlite")
+require("RCurl")
+require("ggplot2")
+df <- data.frame(fromJSON(getURL(URLencode('oraclerest.cs.utexas.edu:5001/rest/native/?query="select * from titanic where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@aevum.cs.utexas.edu:1521/f16pdb', USER='cs329e_rdg935', PASS='orcl_rdg935', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+summary(df)
+head(df)
+
+require(extrafont)
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="Age", y=paste("Fare")) +
+  
+  layer(data=df, 
+        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX),  
+        geom="point",
+        stat="identity",
+        position=position_jitter(width=0.3, height=0)
+  )
